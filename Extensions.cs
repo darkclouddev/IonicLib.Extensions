7f7cff9d22277c62d3dfd0853bb506a68bae4d81
IonPrime <this@ionpri.me>
2018-09-17 04:08:54 +0300

    initial commit

---
+++ Extensions.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace IonicLib.Extensions
+{
+	public static class Extensions
+	{
+		public static T Random<T>(this IEnumerable<T> collection)
+		{
+			return collection.ToArray()[Helper.XorShift() % collection.Count()];
+		}
+
+		public static bool IsWeekEnd(this DateTime dt) =>
+			dt.DayOfWeek == DayOfWeek.Saturday || dt.DayOfWeek == DayOfWeek.Sunday;
+
+		public static TimeSpan GetElapsed(this Action action)
+		{
+			var sw = Stopwatch.StartNew();
+
+			action();
+
+			sw.Stop();
+
+			return sw.Elapsed;
+		}
+
+		public static IEnumerable<T> ForEach<T>(this IEnumerable<T> elements, Action<T> action)
+		{
+			foreach (var element in elements)
+			{
+				action(element);
+			}
+
+			return elements;
+		}
+
+		public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
+		{
+			using (var provider = RandomNumberGenerator.Create())
+			{
+				var list = collection.ToList();
+				var count = list.Count;
+
+				while (count > 1)
+				{
+					var box = new byte[(count / Byte.MaxValue) + 1];
+					int boxSum;
+
+					do
+					{
+						provider.GetBytes(box);
+						boxSum = box.Sum(x => x);
+					}
+					while (!(boxSum < count * ((Byte.MaxValue * box.Length) / count)));
+
+					var pos = (boxSum % count);
+					count--;
+
+					var value = list[pos];
+					list[pos] = list[count];
+					list[count] = value;
+				}
+
+				return list;
+			}
+		}
+
+		/// <param name="min">Inclusive minimum</param>
+		/// <param name="max">Inclusive maximum</param>
+		public static int LimitToRange(this int value, int min, int max)
+		{
+			if (value < min)
+				return min;
+
+			if (value > max)
+				return max;
+
+			return value;
+		}
+
+		public static string FormatTimeToString(this TimeSpan ts)
+		{
+			string result = "";
+
+			if (ts.Days > 0)
+				result += $"{ts.Days} д. ";
+
+			if (ts.Hours > 0)
+				result += $"{ts.Hours} ч. ";
+
+			if (ts.Minutes > 0)
+				result += $"{ts.Minutes} мин. ";
+
+			if (ts.Seconds > 0)
+				result += $"{ts.Seconds} сек.";
+
+			return result;
+		}
+
+		public static string TrimTo(this string str, int maxLength = 50, bool showDots = true)
+		{
+			if (maxLength < 0)
+				throw new ArgumentOutOfRangeException($"");
+
+			if (maxLength == 0)
+				return String.Empty;
+
+			if (maxLength <= 3)
+				return String.Concat(str.Select(x => '.'));
+
+			if (str.Length < maxLength)
+				return str;
+
+			return String.Concat(str.Take(maxLength - 3)) + (showDots ? "..." : "");
+		}
+
+		public static DateTime ToUnixTimestamp(this double number) =>
+			new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(number);
+
+		public static uint AsUnixTimestamp(this DateTime dt) =>
+			(uint)dt.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
+
+		public static ulong ToUnixTimestamp(this DateTime dt)
+		{
+			return (uint)Math.Floor((dt - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds);
+		}
+
+		public static int KiB(this int value) => value * 1024;
+		public static int KB(this int value) => value * 1000;
+
+		public static int MiB(this int value) => value.KiB() * 1024;
+		public static int MB(this int value) => value.KB() * 1000;
+
+		public static int GiB(this int value) => value.MiB() * 1024;
+		public static int GB(this int value) => value.MB() * 1000;
+
+		public static ulong KiB(this ulong value) => value * 1024;
+		public static ulong KB(this ulong value) => value * 1000;
+
+		public static ulong MiB(this ulong value) => value.KiB() * 1024;
+		public static ulong MB(this ulong value) => value.KB() * 1000;
+
+		public static ulong GiB(this ulong value) => value.MiB() * 1024;
+		public static ulong GB(this ulong value) => value.MB() * 1000;
+	}
+}
---
+++ Helper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IonicLib.Extensions
+{
+    public static class Helper
+    {
+		static readonly Random random = new Random();
+
+		const string symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		public static string GenerateConfirmationCode(int length)
+		{
+			var sb = new StringBuilder();
+
+			for (int i = 0; i < length; i++)
+			{
+				sb.Append(symbols.Random());
+			}
+
+			return sb.ToString();
+		}
+
+		public static bool GetChance(uint chance) => GetChance((ulong)chance);
+		public static bool GetChance(ulong chance) => (ulong)random.Next(1, 101) <= chance;
+
+		/// <summary>
+		/// See Random.Next(int,int)
+		/// </summary>
+		/// <param name="min">Minimum value, inclusive</param>
+		/// <param name="max">Maximum value, exclusive</param>
+		/// <returns>A 32-bit unsigned integer greater than or equal to minValue and less than maxValue;
+		/// that is, the range of return values includes minValue but not maxValue.
+		/// If minValue equals maxValue, minValue is returned.
+		/// </returns>
+		public static uint NextUInt(int min, int max) => (uint)random.Next(min, max);
+
+		public static bool GetPreciseChance(float chance) => random.Next(1, 10001) / 100f <= chance;
+
+		static uint x = 548787455, y = 842502087, z = 3579807591, w = 273326509;
+
+		public static uint XorShift()
+		{
+			uint t = x ^ (x << 11);
+			x = y;
+			y = z;
+			z = w;
+
+			return w = w ^ (w >> 19) ^ t ^ (t >> 8);
+		}
+
+		public static ulong CurrentUnixTimestamp => (ulong)Math.Floor((DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds);
+
+		public static DateTime FromTimestamp(ulong unixTimestamp)
+		{
+			return new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(unixTimestamp);
+		}
+
+		const int ProcessorCountRefreshIntervalMs = 30000;
+
+		static volatile int _processorCount;
+		static volatile int _lastProcessorCountRefreshTicks;
+
+		internal static int ProcessorCount
+		{
+			get
+			{
+				int now = Environment.TickCount;
+
+				if (_processorCount == 0 || now - _lastProcessorCountRefreshTicks >= ProcessorCountRefreshIntervalMs)
+				{
+					_processorCount = Environment.ProcessorCount;
+					_lastProcessorCountRefreshTicks = now;
+				}
+
+				return _processorCount;
+			}
+		}
+
+		public async static Task<string> PostAsync(string address, Dictionary<string, string> parameters)
+		{
+			using (var client = new HttpClient())
+			{
+				var @params = new FormUrlEncodedContent(parameters);
+
+				var response = await client.PostAsync(address, @params);
+
+				return await response.Content.ReadAsStringAsync();
+			}
+		}
+	}
+}
